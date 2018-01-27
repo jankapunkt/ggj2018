51554136d72406c9be634c858bda7d0ffa5abc50
jankapunkt <jkuester@uni-bremen.de>
2018-01-27 22:15:18 +0100

    retry game

--- app/client/main.js
+++ app/client/main.js
@@ -5,8 +5,9 @@ import './main.css';
 import {ReactiveDict} from 'meteor/reactive-dict';
 
 state = new ReactiveDict();
-state.set('rescuers', 0);
-state.set('timer', null);
+state.set('rescuers', 1);
+state.set('timer', 300);
+state.set('enemies', 1);
 
 Template.registerHelper('state', function (key) {
     return state.get(key);
@@ -17,7 +18,27 @@ Template.body.events({
         event.preventDefault();
         state.set('isRunning', true);
         initGame();
+    },
+
+    'click #retry-level-button'(event) {
+        event.preventDefault();
+        cleanupGame();
+    },
+
+    'click #next-level-button'(event) {
+        event.preventDefault();
+        nextLevel();
+        cleanupGame();
     }
-})
+});
+
+function nextLevel() {
+    const tmr = state.get("timer");
+    state.set("timer", Math.round(tmr * 0.9));
+    const rsc = state.get("rescuers");
+    state.set("rescuers", Math.round(rsc * 1.6));
+    const enm = state.get("enemies");
+    state.set("rescuers", Math.round(enm * 1.9));
+}
 
 import './main.html';
